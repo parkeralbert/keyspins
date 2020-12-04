2691fc9bf5e725ae5ba15739485ee992d43222e9
parkeralbert <parkeralbert@192.168.0.137>
2020-12-04 12:32:25 -0600

    getSpinData moved to child class

--- spinsearch/src/SpinSearch.java
+++ spinsearch/src/SpinSearch.java
@@ -18,7 +18,7 @@ import org.jsoup.select.Elements;
 
 public class SpinSearch {
 
-	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
 
 	
 	public static Date parseDate(String date) {
@@ -178,6 +178,7 @@ public class SpinSearch {
 		
 		spinData = (page.select(String.format("td:containsOwn(%s)", songOrAlbumName)));
 		System.out.println("*** Retrieved " + artist + " spins: " + spinData.text());
+		
 		
 		return spinData;
 	}
--- spinsearch/src/XpnSearch.java
+++ spinsearch/src/XpnSearch.java
@@ -1,4 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class XpnSearch extends SpinSearch {
-
+	private	static Elements getSpinData(ArtistInfo currentArtist, String url, String songOrAlbumName) throws Exception {
+		Map<String, String> postData = new HashMap<>();
+		String artist = (String) currentArtist.getArtistName();
+		postData.put("val", "search");
+		postData.put("search", artist);
+		postData.put("playlist", "all");
+		
+		Document page = Jsoup.connect(url).userAgent(USER_AGENT).data(postData).post();
+		
+		Elements spinData = null;
+		
+		spinData = (page.select(String.format("td:containsOwn(%s)", songOrAlbumName)));
+		System.out.println("*** Retrieved " + artist + " spins: " + spinData.text());
+		
+		
+		return spinData;
+	}
 }
